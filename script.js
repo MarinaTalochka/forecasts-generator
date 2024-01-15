18f26da28455a833d0fba99e27fc3e81d6db2ff1
MarinaTalochka <153221203+MarinaTalochka@users.noreply.github.com>
2024-01-15 22:05:32 +0300

    ДЗ\5

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-/* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
 
-/* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
+const button = document.querySelector(".forecast-btn");
+const predictionTitle = document.querySelector('.current-forecast h1');
+const probabilityTitle = document.querySelector('.current-forecast p');
+const forecastsContainer = document.querySelector('.forecasts');
+const forecastTemplate = document.querySelector('#forecast-item');
 
-/* Подставляй текст нового предсказания в .current-forecast h1 */
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-/* Показывай процент вероятности, с которым предсказание сбудется — в верстке это .current-forecast p */
+function generatePrediction () {
+    const predictions = [
+        "Сегодня тебе повезет в любви!",
+        "Тебя ждет успех в бизнесе!",
+        "Скоро ты найдешь новых друзей!",
+        "Тебе стоит обратить внимание на свое здоровье!",
+        "Ты скоро получишь хорошую новость!"
+        ];
 
-/* Данный процент также нужно генерировать автоматически, он может принимать значения от 0 до 100% */
-
-/* Совет: заведи функцию-хелпер, которая будет заниматься только генерацией данных в диапазоне от min до max и используй ее где нужно */
-
-/* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
-
-/* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+    const randomPrediction = predictions[generateRandomNumber(0, predictions.length - 1)];
+    const randomProbability = generateRandomNumber(0, 100);
+      
+    predictionTitle.textContent = randomPrediction;
+    probabilityTitle.textContent =`Вероятность события; ${randomProbability}%`;
+    }
+    
+function savePredictionToList() {
+    const title = predictionTitle.textContent;
+    const probability = probabilityTitle.textContent;
+    
+    if (!title || !probability) {
+      return;
+    }
+    
+    const forecastToAdd = forecastTemplate.content.cloneNode(true);
+    forecastToAdd.querySelector('h3').textContent = title;
+    forecastToAdd.querySelector('p').textContent = probability;
+    forecastsContainer.prepend(forecastToAdd);
+}
+    
+button.addEventListener("click", function(){
+    savePredictionToList();
+    generatePrediction();
+});
